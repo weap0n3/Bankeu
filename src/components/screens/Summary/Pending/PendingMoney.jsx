7cda8a1d9f2ff7a5b4c484427ea1a88b48d5bc4e
weap0n3 <oleksiybiliy2006@gmail.com>
2024-01-02 06:19:53 +0100

    add styles to Pendingmoney

--- src/components/screens/Summary/Pending/PendingMoney.jsx
+++ src/components/screens/Summary/Pending/PendingMoney.jsx
@@ -4,12 +4,16 @@ import styles from './PendingMoney.module.css'
 const PendingMoney = () => {
 	return (
 		<section className={styles.bottombar}>
-			<div className={styles.info}>
-				<img src='' alt='' />
-				<p className={styles.text}>
-					You have USD 1.000 pending money, it will be ready in 2 business days.
-				</p>
-				<a href=''>Get your money now</a>
+			<div className={styles.image}>
+				<div className={styles.info}>
+					<p className={styles.text}>
+						You have USD 1.000 pending money, <br /> it will be ready in 2
+						business days.
+					</p>
+					<a id={styles.money} href=''>
+						Get your money now
+					</a>
+				</div>
 			</div>
 		</section>
 	)
