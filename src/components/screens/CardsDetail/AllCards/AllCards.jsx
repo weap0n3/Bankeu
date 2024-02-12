d1b6af79540e48ac72203039ae5fd940c6597999
weap0n3 <weap0n1823@gmail.com>
2024-02-12 15:35:17 +0100

    refactoring CardsDetail

--- src/components/screens/CardsDetail/AllCards/AllCards.jsx
+++ src/components/screens/CardsDetail/AllCards/AllCards.jsx
@@ -1,6 +1,27 @@
-import Card from './Card/Card'
+import Card from '../Card/Card'
+import styles from './AllCards.module.css'
 
-const AllCards = () => {
+const AllCards = ({ cards }) => {
+	const formatDateOfEndInput = e => {
+		const regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
+		if (regex.test(e)) {
+			const [year, month] = e.split('-')
+			const formattedDateString = `${month}/${year.slice(2)}`
+			return formattedDateString
+		} else {
+			return e
+		}
+	}
+
+	const formatCardNumber = e => {
+		if (e) {
+			const formattedValue = e.replace(/\s/g, '').match(/.{1,4}/g)
+			return formattedValue ? formattedValue.join(' ') : ''
+		} else {
+			return e
+		}
+	}
+
 	return (
 		<div className={styles.card_container}>
 			{cards.map(card => (
--- src/components/screens/CardsDetail/CardsDetail.jsx
+++ src/components/screens/CardsDetail/CardsDetail.jsx
@@ -2,31 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { CardService } from '../../../services/card.service'
 import Cards from '../../ui/Cards/Cards'
 import Header from '../../ui/Header/Header'
-import Card from './Card/Card'
+import AllCards from './AllCards/AllCards'
 import styles from './CardsDetail.module.css'
-import AddCard from '../../ui/AddCard/AddCard'
 
 const CardsDetail = () => {
 	const [cards, setCards] = useState([])
-	const formatDateOfEndInput = e => {
-		const regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
-		if (regex.test(e)) {
-			const [year, month] = e.split('-')
-			const formattedDateString = `${month}/${year.slice(2)}`
-			return formattedDateString
-		} else {
-			return e
-		}
-	}
-
-	const formatCardNumber = e => {
-		if (e) {
-			const formattedValue = e.replace(/\s/g, '').match(/.{1,4}/g)
-			return formattedValue ? formattedValue.join(' ') : ''
-		} else {
-			return e
-		}
-	}
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -44,16 +24,7 @@ const CardsDetail = () => {
 					<Cards setCards={setCards} />
 				</div>
 				<section className={styles.main_content}>
-					<div className={styles.card_container}>
-						{cards.map(card => (
-							<Card
-								key={card._id}
-								card={card}
-								formatCardNumber={formatCardNumber}
-								formatDateOfEndInput={formatDateOfEndInput}
-							/>
-						))}
-					</div>
+					<AllCards cards={cards} />
 				</section>
 			</main>
 		</wrapper>
