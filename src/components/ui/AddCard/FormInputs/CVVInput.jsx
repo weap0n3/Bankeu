ae0ea6747581fb482b96434c58784d2d2c43e98c
weap0n3 <weap0n1823@gmail.com>
2024-04-01 15:06:50 +0200

    create CvvInput.jsx

---
+++ src/components/ui/AddCard/FormInputs/CVVInput.jsx
@@ -0,0 +1,21 @@
+import { useAddCard } from '../../../../hooks/useAddCard'
+import styles from '../AddCard.module.css'
+
+const CVVInput = ({ data, setData, handleCVVFocus, handleCVVUnFocus }) => {
+	return (
+		<input
+			id={styles.cvv}
+			className={styles.inputs}
+			placeholder='CVV'
+			onChange={e => setData(prev => ({ ...prev, CVV: e.target.value }))}
+			value={data.CVV}
+			pattern='\d{3}'
+			maxLength={3}
+			onFocus={handleCVVFocus}
+			onBlur={handleCVVUnFocus}
+			required
+		/>
+	)
+}
+
+export default CVVInput
