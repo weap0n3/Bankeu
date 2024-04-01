764700ce56b6a08b511f2f6296219abb694dddfa
weap0n3 <weap0n1823@gmail.com>
2024-04-01 15:07:09 +0200

    create FateOfEndInput.jsx

---
+++ src/components/ui/AddCard/FormInputs/DateOfEndInput.jsx
@@ -0,0 +1,24 @@
+import styles from '../AddCard.module.css'
+import { applyFormat } from '../addCard-formats'
+
+const DateOfEndInput = ({ data, setData }) => {
+	return (
+		<input
+			id={styles.date}
+			className={styles.inputs}
+			type='data'
+			placeholder='Expiry Date'
+			onChange={e =>
+				setData(prev => ({
+					...prev,
+					dateOfEnd: applyFormat(e.target.value, 'DateOfEnd'),
+				}))
+			}
+			value={applyFormat(data.dateOfEnd, 'DateOfEndInput')}
+			pattern='(0[1-9]|1[0-2])\/\d{2}'
+			required
+		/>
+	)
+}
+
+export default DateOfEndInput
